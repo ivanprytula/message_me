434d515383a03ce8b29a9bd7c61ca3e77c34c703
ivanprytula <ivanprytula87@gmail.com>
2022-06-18 00:21:08 +0300

    Display messages using partial

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,8 @@ class MessagesController < ApplicationController
     if message.save
       # redirect_to root_path
       ActionCable.server.broadcast "chatroom_channel",
-                                   foo: message.body
+                                   modified_message: message_render(message)
+
     end
   end
 
@@ -14,6 +15,10 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:body)
+  end
+
+  def message_render(message)
+    render(partial: 'message', locals: { message: message })
   end
 
 end
