7f414bb9a8a7061754855b98f20f20b0b8f89d59
ivanprytula <ivanprytula87@gmail.com>
2022-06-17 16:54:14 +0300

    Enable flash messages display

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       redirect_to root_path
     else
       flash.now[:error] = 'There was something wrong with login info'
-      render :new
+      render 'new'
     end
   end
 
