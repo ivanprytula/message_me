8833bf05bb0274c576952fae117c9e383ed2bc4d
ivanprytula <ivanprytula87@gmail.com>
2022-06-18 00:53:52 +0300

    Beautify input box, functionality and create custom scope

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,6 +26,15 @@ scroll_bottom = function () {
   }
 }
 
+submit_message = function () {
+  $('#message_body').on('keydown', function (e) {
+    if (e.keyCode === 13) {
+      $('button').click();
+      e.target.value = "";
+    }
+  });
+};
+
 $(document).on('turbolinks:load', function () {
   $('.ui.dropdown').dropdown();
 
@@ -33,6 +42,7 @@ $(document).on('turbolinks:load', function () {
     $(this).closest('.message').transition('fade');
   });
 
+  submit_message();
   scroll_bottom();
 })
 
